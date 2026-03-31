fcf65b4426e4563dc4bbe116c9f3cad37b82ce8d
Vincius <viniviniee11@gmail.com>
2026-03-31 15:50:06 -0300

    feat: cria o model do semestre

--- api-2sem/src/main/java/org/datasphere/model/SemestreModel.java
+++ api-2sem/src/main/java/org/datasphere/model/SemestreModel.java
@@ -1,4 +1,43 @@
 package org.datasphere.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class SemestreModel {
+
+    private LocalDate diaInicio;
+    private LocalDate diaFim;
+    private List aulasPlanejadas;
+
+    public SemestreModel() {
+    }
+
+    public SemestreModel(LocalDate diaInicio, LocalDate diaFim) {
+        this.diaInicio = diaInicio;
+        this.diaFim = diaFim;
+    }
+
+    public LocalDate getDiaInicio() {
+        return diaInicio;
+    }
+
+    public void setDiaInicio(LocalDate diaInicio) {
+        this.diaInicio = diaInicio;
+    }
+
+    public LocalDate getDiaFim() {
+        return diaFim;
+    }
+
+    public void setDiaFim(LocalDate diaFim) {
+        this.diaFim = diaFim;
+    }
+
+    public List getAulasPlanejadas() {
+        return aulasPlanejadas;
+    }
+
+    public void setAulasPlanejadas(List aulasPlanejadas) {
+        this.aulasPlanejadas = aulasPlanejadas;
+    }
 }
