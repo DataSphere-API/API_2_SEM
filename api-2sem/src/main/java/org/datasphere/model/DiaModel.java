644d4db0473c4fb2aaf4b43a20ae64459fa0e52a
Vincius <viniviniee11@gmail.com>
2026-03-31 15:49:31 -0300

    feat: cria o model do dia

--- api-2sem/src/main/java/org/datasphere/model/DiaModel.java
+++ api-2sem/src/main/java/org/datasphere/model/DiaModel.java
@@ -1,4 +1,31 @@
 package org.datasphere.model;
 
 public class DiaModel {
+
+    private LocalDate data;
+    private DiaEnum tipo;
+
+    public DiaModel() {
+    }
+
+    public DiaModel(LocalDate data, DiaEnum tipo) {
+        this.data = data;
+        this.tipo = tipo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public DiaEnum getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(DiaEnum tipo) {
+        this.tipo = tipo;
+    }
 }
