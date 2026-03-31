2039b892910e561f4cf42ee8c4b8e1e379aae732
Vincius <viniviniee11@gmail.com>
2026-03-31 15:49:50 -0300

    feat: cria o model da aula planejada

--- api-2sem/src/main/java/org/datasphere/model/AulaPlanejada.java
+++ api-2sem/src/main/java/org/datasphere/model/AulaPlanejada.java
@@ -1,4 +1,69 @@
 package org.datasphere.model;
 
+import org.datasphere.enums.DiaEnum;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class AulaPlanejada {
+
+    private AulaModel aulaModel;
+    private TopicoModel topicoModel;
+    private DiaModel diaModel;
+    private LocalDate data;
+
+    public AulaPlanejada() {
+    }
+
+    public AulaPlanejada(AulaModel aulaModel, TopicoModel topicoModel, DiaModel diaModel, LocalDate data) {
+        this.aulaModel = aulaModel;
+        this.topicoModel = topicoModel;
+        this.diaModel = diaModel;
+        this.data = data;
+    }
+
+    public AulaPlanejada(DiaModel diaModel, TopicoModel topicoModel, AulaModel aulaModel) {
+        this.diaModel = diaModel;
+        this.topicoModel = topicoModel;
+        this.aulaModel = aulaModel;
+    }
+
+    public AulaModel getAulaModel() {
+        return aulaModel;
+    }
+
+    public void setAulaModel(AulaModel aulaModel) {
+        this.aulaModel = aulaModel;
+    }
+
+    public TopicoModel getTopicoModel() {
+        return topicoModel;
+    }
+
+    public void setTopicoModel(TopicoModel topicoModel) {
+        this.topicoModel = topicoModel;
+    }
+
+    public DiaModel getDiaModel() {
+        return diaModel;
+    }
+
+    public void setDiaModel(DiaModel diaModel) {
+        this.diaModel = diaModel;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public boolean isDiaLetivo() {
+        return diaModel.getTipo() != DiaEnum.NAO_LETIVO;
+    }
+
 }
