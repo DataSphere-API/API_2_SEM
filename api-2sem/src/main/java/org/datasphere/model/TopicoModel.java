2145bee12aca27e2ece051029677166883f64666
Vincius <viniviniee11@gmail.com>
2026-03-31 15:50:11 -0300

    feat: cria o model do topico

--- api-2sem/src/main/java/org/datasphere/model/TopicoModel.java
+++ api-2sem/src/main/java/org/datasphere/model/TopicoModel.java
@@ -1,4 +1,42 @@
 package org.datasphere.model;
 
 public class TopicoModel {
+
+    private Long id;
+    private String titulo;
+    private Integer aulasNecessarias;
+
+    public TopicoModel() {
+    }
+
+    public TopicoModel(String titulo, Integer aulasNecessarias) {
+        this.titulo = titulo;
+        this.aulasNecessarias = aulasNecessarias;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getAulasNecessarias() {
+        return aulasNecessarias;
+    }
+
+    public void setAulasNecessarias(Integer aulasNecessarias) {
+        this.aulasNecessarias = aulasNecessarias;
+    }
+
+
 }
