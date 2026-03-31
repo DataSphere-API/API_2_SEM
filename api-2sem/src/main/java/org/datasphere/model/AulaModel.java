aa5f50057252589f974d3963ca6947f112543ad0
Vincius <viniviniee11@gmail.com>
2026-03-31 15:49:44 -0300

    feat: cria o model da aula

--- api-2sem/src/main/java/org/datasphere/model/AulaModel.java
+++ api-2sem/src/main/java/org/datasphere/model/AulaModel.java
@@ -1,4 +1,44 @@
 package org.datasphere.model;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
 public class AulaModel {
+
+    private DayOfWeek diaDaSemana;
+    private LocalTime horaInicio;
+    private LocalTime horaFim;
+
+    public AulaModel(DayOfWeek diaDaSemana, LocalTime horaInicio, LocalTime horaFim) {
+        this.diaDaSemana = diaDaSemana;
+        this.horaInicio = horaInicio;
+        this.horaFim = horaFim;
+    }
+
+    public AulaModel() {
+    }
+
+    public DayOfWeek getDiaDaSemana() {
+        return diaDaSemana;
+    }
+
+    public void setDiaDaSemana(DayOfWeek diaDaSemana) {
+        this.diaDaSemana = diaDaSemana;
+    }
+
+    public LocalTime getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(LocalTime horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public LocalTime getHoraFim() {
+        return horaFim;
+    }
+
+    public void setHoraFim(LocalTime horaFim) {
+        this.horaFim = horaFim;
+    }
 }
