5e9683aa092a1ef9cea981bb1f4c1c38dcc0164b
Vincius <viniviniee11@gmail.com>
2026-03-31 15:52:12 -0300

    fix: adiciona suporte ao javafx.base

--- api-2sem/src/main/java/module-info.java
+++ api-2sem/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module org.datasphere {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens org.datasphere to javafx.fxml;
+    opens org.datasphere to javafx.base, javafx.fxml;
     exports org.datasphere;
     exports org.datasphere.controller;
     opens org.datasphere.controller to javafx.fxml;
