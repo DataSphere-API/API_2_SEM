16d6eebe47d67a95e68a71c16b392b0089ea039b
Vincius <viniviniee11@gmail.com>
2026-04-01 13:46:50 -0300

    feat: altera o método organizarAulas() e integra com a tableView de aulas planejadas

--- api-2sem/src/main/java/org/datasphere/controller/CadastrarAulaController.java
+++ api-2sem/src/main/java/org/datasphere/controller/CadastrarAulaController.java
@@ -2,6 +2,7 @@ package org.datasphere.controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -13,9 +14,12 @@ import java.io.IOException;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 
 public class CadastrarAulaController {
 
@@ -78,11 +82,10 @@ public class CadastrarAulaController {
         clnTopico.setCellValueFactory(new PropertyValueFactory<>("titulo"));
         clnAulasTopico.setCellValueFactory(new PropertyValueFactory<>("aulasNecessarias"));
 
-        configurarTabela();
-        criarSemestreComDias();
+        configurarTabelaTopicos();
     }
 
-    private void configurarTabela(){
+    private void configurarTabelaTopicos(){
 
         clnIdTopico.setCellValueFactory(cellData ->
                 new javafx.beans.property.SimpleObjectProperty<>(cellData.getValue().getId())
@@ -120,48 +123,71 @@ public class CadastrarAulaController {
      APÓS ISSO, ELE DEVE SER ALTERADO SEM FALTA!!!!
     **/
     public void criarSemestreComDias(){
-        SemestreModel semestre = new SemestreModel(LocalDate.of(2026,04,01), LocalDate.of(2026,04,30));
-        DiaModel diaLetivo = new DiaModel(LocalDate.of(2026,04,1), DiaEnum.LETIVO);
-        DiaModel diaLetivo1 = new DiaModel(LocalDate.of(2026,04,2), DiaEnum.LETIVO);
-        DiaModel diaLetivo2 = new DiaModel(LocalDate.of(2026,04,6), DiaEnum.LETIVO);
-        DiaModel diaLetivo3 = new DiaModel(LocalDate.of(2026,04,7), DiaEnum.LETIVO);
-        DiaModel diaLetivo4 = new DiaModel(LocalDate.of(2026,04,8), DiaEnum.LETIVO);
-        DiaModel diaLetivo5 = new DiaModel(LocalDate.of(2026,04,9), DiaEnum.LETIVO);
-        DiaModel diaLetivo7 = new DiaModel(LocalDate.of(2026,04,10), DiaEnum.LETIVO);
+        semestre.adicionarDias(new DiaModel(LocalDate.of(2026,04,1), DiaEnum.LETIVO));
+        semestre.adicionarDias(new DiaModel(LocalDate.of(2026,04,2), DiaEnum.LETIVO));
+        semestre.adicionarDias(new DiaModel(LocalDate.of(2026,04,6), DiaEnum.LETIVO));
+        semestre.adicionarDias(new DiaModel(LocalDate.of(2026,04,7), DiaEnum.LETIVO));
+        semestre.adicionarDias(new DiaModel(LocalDate.of(2026,04,8), DiaEnum.LETIVO));
+        semestre.adicionarDias(new DiaModel(LocalDate.of(2026,04,9), DiaEnum.LETIVO));
+        semestre.adicionarDias(new DiaModel(LocalDate.of(2026,04,10), DiaEnum.LETIVO));
 
-        DiaModel diaNaoLetivo = new DiaModel(LocalDate.of(2026,04,03), DiaEnum.NAO_LETIVO);
-        DiaModel diaNaoLetivo1 = new DiaModel(LocalDate.of(2026,04,04), DiaEnum.NAO_LETIVO);
-        DiaModel diaNaoLetivo2 = new DiaModel(LocalDate.of(2026,04,05), DiaEnum.NAO_LETIVO);
+        semestre.adicionarDias(new DiaModel(LocalDate.of(2026,04,03), DiaEnum.NAO_LETIVO));
+        semestre.adicionarDias(new DiaModel(LocalDate.of(2026,04,04), DiaEnum.NAO_LETIVO));
+        semestre.adicionarDias(new DiaModel(LocalDate.of(2026,04,05), DiaEnum.NAO_LETIVO));
     }
 
-    public AulaModel lerDiaHorarioAula(){
+    public List<AulaModel> lerDiaHorarioAula(){
+        List<AulaModel> diasDeAula = new LinkedList<>();
+
         if (chkSegunda.isSelected() && chkSeg1845.isSelected() && chkSeg1935.isSelected()){
-            AulaModel aulaSegunda = new AulaModel(DayOfWeek.MONDAY, LocalTime.of(18,45), LocalTime.of(19,35));
-            chkSeg1935.setIndeterminate(true);
-            chkSeg1935.setIndeterminate(true);
-            chkSegunda.setIndeterminate(true);
-            return aulaSegunda;
+            diasDeAula.add(new AulaModel(DayOfWeek.MONDAY, LocalTime.of(18,45), LocalTime.of(19,35)));
         }
-        return null;
+        if (chkTerca.isSelected() && chkTer1845.isSelected() && chkTer1935.isSelected()){
+            diasDeAula.add(new AulaModel(DayOfWeek.TUESDAY, LocalTime.of(18,45), LocalTime.of(19,35)));
+        }
+        if (chkQuarta.isSelected() && chkQua1845.isSelected() && chkQua1935.isSelected()){
+            diasDeAula.add(new AulaModel(DayOfWeek.WEDNESDAY, LocalTime.of(18,45), LocalTime.of(19,35)));
+        }
+        if (chkQuinta.isSelected() && chkQui1845.isSelected() && chkQui1935.isSelected()){
+            diasDeAula.add(new AulaModel(DayOfWeek.THURSDAY, LocalTime.of(18,45), LocalTime.of(19,35)));
+        }
+        if (chkSexta.isSelected() && chkSex1845.isSelected() && chkSex1935.isSelected()){
+            diasDeAula.add(new AulaModel(DayOfWeek.FRIDAY, LocalTime.of(18,45), LocalTime.of(19,35)));
+        }
+
+        return diasDeAula;
     }
 
-    public List<AulaPlanejada> organizarAulas(List<AulaPlanejada> aulas, SemestreModel semestreModel) {
+    public List<AulaPlanejada> organizarAulas(List<TopicoModel> topicos,List<AulaModel> aulas, SemestreModel semestreModel) {
         List<AulaPlanejada> planejamentoAulas = new LinkedList<>();
 
+        int topicoAtualIndex = 0;
+        int aulasNoTopicoAtual = 0;
+
         for (LocalDate dia = semestreModel.getDiaInicio(); !dia.isAfter(semestreModel.getDiaFim()); dia = dia.plusDays(1)) {
-            for (AulaPlanejada aula : aulas) {
+            for (AulaModel aula : aulas) {
 
-                boolean diaDaSemanaCorreto = dia.getDayOfWeek() == aula.getAulaModel().getDiaDaSemana();
-                boolean diaLetivo = aula.isDiaLetivo();
+                boolean diaDaSemanaCorreto = dia.getDayOfWeek() == aula.getDiaDaSemana();
+                LocalDate finalDia = dia;
+                boolean diaLetivo = semestreModel.getDiasList().stream()
+                        .anyMatch(d -> d.getData().equals(finalDia) && d.getTipo() == DiaEnum.LETIVO);
 
-                if (diaDaSemanaCorreto && diaLetivo) {
-                    AulaPlanejada aulaComData = new AulaPlanejada(
-                            aula.getDiaModel(),
-                            aula.getTopicoModel(),
-                            aula.getAulaModel()
-                    );
+                if (diaDaSemanaCorreto && diaLetivo && topicoAtualIndex < topicos.size()) {
+
+                    TopicoModel topicoAtual = topicos.get(topicoAtualIndex);
+
+                    AulaPlanejada aulaComData = new AulaPlanejada();
+                    aulaComData.setAulaModel(aula);
                     aulaComData.setDiaModel(new DiaModel(dia, DiaEnum.LETIVO));
+                    aulaComData.setTopicoModel(topicoAtual);
+
                     planejamentoAulas.add(aulaComData);
+
+                    aulasNoTopicoAtual++;
+                    if (aulasNoTopicoAtual >= topicoAtual.getAulasNecessarias()) {
+                        topicoAtualIndex++;
+                        aulasNoTopicoAtual = 0;
+                    }
                 }
             }
         }
@@ -169,11 +195,31 @@ public class CadastrarAulaController {
         return planejamentoAulas;
     }
 
+    private void adicionarTopicoLista(){
+        List<AulaPlanejada> aulasPlanejadas = organizarAulas(obsListTopicos, lerDiaHorarioAula(), semestre);
 
 
+            clnAulasPlanejamento.setCellValueFactory(cellData ->
+                    new javafx.beans.property.SimpleStringProperty((cellData.getValue().getAulaModel().getDiaDaSemana().getDisplayName(TextStyle.SHORT, new Locale("pt", "BR")) + " "
+                            + (cellData.getValue().getAulaModel().getHoraInicio()+ " - "))
+                            + (cellData.getValue().getAulaModel().getHoraFim()))
 
+            );
 
+            clnTopicoPlanejamento.setCellValueFactory(cellData ->
+                    new javafx.beans.property.SimpleStringProperty(cellData.getValue().getTopicoModel().getTitulo())
+            );
 
+            clnDataPlanejamento.setCellValueFactory(cellData ->
+                    new javafx.beans.property.SimpleStringProperty((cellData.getValue().getDiaModel().getData().format(fmt)))
+            );
 
+        tblPlanejamentoAulas.setItems(FXCollections.observableArrayList(aulasPlanejadas));
+    }
+
+    @FXML
+    private void gerarPlanejamentoPlanilha(ActionEvent event){
+        adicionarTopicoLista();
+    }
 
 }
