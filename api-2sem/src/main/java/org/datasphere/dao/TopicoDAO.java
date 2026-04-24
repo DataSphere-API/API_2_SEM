5a53a6c7f30b85b6025a88599b4ea86db6452f03
Vincius <viniviniee11@gmail.com>
2026-04-23 21:14:18 -0300

    feat: cria o TopicoDAO implementando os métodos do IDAO

---
+++ api-2sem/src/main/java/org/datasphere/dao/TopicoDAO.java
@@ -0,0 +1,63 @@
+package org.datasphere.dao;
+
+import org.datasphere.dao.interfaces.IDAO;
+import org.datasphere.database.ConexaoDB;
+import org.datasphere.model.TopicoModel;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TopicoDAO implements IDAO <TopicoModel> {
+
+    @Override
+    public void salvar(TopicoModel topico){
+        String sql = "INSERT INTO topico (titulo, aulasNecessarias) VALUES (?,?)";
+        try(Connection conn = ConexaoDB.getConexao()){
+
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setString(1,topico.getTitulo());
+            ps.setInt(2,topico.getAulasNecessarias());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<TopicoModel> listar(){
+        String sql = "SELECT * FROM topico ORDER BY ID";
+        List<TopicoModel> topicoModelList = new ArrayList<>();
+        try(Connection conn = ConexaoDB.getConexao()){
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                TopicoModel topicoModel = new TopicoModel();
+                topicoModel.setId(rs.getLong("id"));
+                topicoModel.setTitulo(rs.getString("titulo"));
+                topicoModel.setAulasNecessarias(rs.getInt("aulasNecessarias"));
+
+                topicoModelList.add(topicoModel);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return topicoModelList;
+    }
+
+    @Override
+    public void deletar(Long id){
+        String sql = "DELETE FROM topico WHERE id = ?";
+        try(Connection conn = ConexaoDB.getConexao()){
+
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setLong(1, id);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
