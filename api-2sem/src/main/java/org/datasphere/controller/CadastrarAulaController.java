f01eada62f61683692f7e56e5d922c858e5ccc15
Ieda Moretini <imoretini@hotmail.com>
2026-04-24 20:58:34 -0300

    style: add checkBox de prova

--- api-2sem/src/main/java/org/datasphere/controller/CadastrarAulaController.java
+++ api-2sem/src/main/java/org/datasphere/controller/CadastrarAulaController.java
@@ -23,6 +23,9 @@ public class CadastrarAulaController {
     private TableColumn<TopicoModel, Long> clnIdTopico;
 
     @FXML
+    private CheckBox chkProva;
+
+    @FXML
     private TableColumn<TopicoModel, String> clnTopico;
 
     @FXML
