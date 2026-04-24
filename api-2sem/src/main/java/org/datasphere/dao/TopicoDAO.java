7210c483f225eedba83d6191f8daec45c7186657
Vincius <viniviniee11@gmail.com>
2026-04-24 10:17:52 -0300

    fix: adiciona o método executeUpdate ao preparedStatement

--- api-2sem/src/main/java/org/datasphere/dao/TopicoDAO.java
+++ api-2sem/src/main/java/org/datasphere/dao/TopicoDAO.java
@@ -20,6 +20,8 @@ public class TopicoDAO implements IDAO <TopicoModel> {
             ps.setString(1,topico.getTitulo());
             ps.setInt(2,topico.getAulasNecessarias());
 
+            ps.executeUpdate();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -54,6 +56,8 @@ public class TopicoDAO implements IDAO <TopicoModel> {
 
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setLong(1, id);
+
+            ps.executeUpdate();
 
         } catch (SQLException e) {
             e.printStackTrace();
