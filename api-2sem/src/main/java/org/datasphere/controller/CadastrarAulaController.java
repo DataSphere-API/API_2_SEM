d98afe4ad6c075b1d5a94ba2d790202aed2631e3
luiz-andrade1 <luizfelipequeirosandrade@gmail.com>
2026-04-28 21:28:56 -0300

    refactor: remove o metodo organizarAula da controller

--- api-2sem/src/main/java/org/datasphere/controller/CadastrarAulaController.java
+++ api-2sem/src/main/java/org/datasphere/controller/CadastrarAulaController.java
@@ -203,44 +203,6 @@ public class CadastrarAulaController {
         return diasDeAula;
     }
 
-    public List<AulaPlanejada> organizarAulas(List<TopicoModel> topicos,List<AulaModel> aulas, SemestreModel semestreModel) {
-        List<AulaPlanejada> planejamentoAulas = new LinkedList<>();
-
-        int topicoAtualIndex = 0;
-        int aulasNoTopicoAtual = 0;
-
-        for (LocalDate dia = semestreModel.getDiaInicio(); !dia.isAfter(semestreModel.getDiaFim()); dia = dia.plusDays(1)) {
-            for (AulaModel aula : aulas) {
-
-                boolean diaDaSemanaCorreto = dia.getDayOfWeek() == aula.getDiaDaSemana();
-                LocalDate finalDia = dia;
-                boolean diaLetivo = semestreModel.getDiasList().stream()
-                        .anyMatch(d -> d.getData().equals(finalDia));
-
-                if (diaDaSemanaCorreto && topicoAtualIndex < topicos.size()) {
-
-                    TopicoModel topicoAtual = topicos.get(topicoAtualIndex);
-
-                    AulaPlanejada aulaComData = new AulaPlanejada();
-                    aulaComData.setAulaModel(aula);
-                    aulaComData.setDiaModel(new DiaModel(dia, true));
-                    aulaComData.setTopicoModel(topicoAtual);
-
-                    planejamentoAulas.add(aulaComData);
-                    aulaPlanejadaDAO.salvar(aulaComData);
-
-                    aulasNoTopicoAtual++;
-                    if (aulasNoTopicoAtual >= topicoAtual.getAulasNecessarias()) {
-                        topicoAtualIndex++;
-                        aulasNoTopicoAtual = 0;
-                    }
-                }
-            }
-        }
-
-        return planejamentoAulas;
-    }
-
     private void adicionarTopicoLista(){
         List<AulaPlanejada> aulasPlanejadas = organizarAulas(obsListTopicos, lerDiaHorarioAula(), semestreService.getSemestre());
 
