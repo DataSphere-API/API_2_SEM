a9be632f6117c5acd2fae87760b0c83c81919aa0
Vincius <viniviniee11@gmail.com>
2026-05-12 22:21:54 -0300

    chore: importa o ResultSet da classe ResultSet

--- api-2sem/src/main/java/org/datasphere/model/LoginDAO.java
+++ api-2sem/src/main/java/org/datasphere/model/LoginDAO.java
@@ -5,6 +5,7 @@ import org.datasphere.service.SenhaHashService;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class LoginDAO {
