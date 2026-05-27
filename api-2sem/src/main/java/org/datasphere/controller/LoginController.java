0e3c99023b832e946eaa048d41f959a59ca84a1a
Ieda Moretini <imoretini@hotmail.com>
2026-05-27 14:51:09 -0300

    Style: implementa funcionalidade nos links 'Criar conta' da tela de login e 'entrar' da tela de cadastro de usuario

--- api-2sem/src/main/java/org/datasphere/controller/LoginController.java
+++ api-2sem/src/main/java/org/datasphere/controller/LoginController.java
@@ -2,11 +2,18 @@ package org.datasphere.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
@@ -30,16 +37,27 @@ public class LoginController {
 
     @FXML
     void criarConta(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/datasphere/cadastroUsua.fxml"));
+
+
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            stage.setScene(new Scene(root));
+            stage.setTitle("SIGA.ME - Planejamento de Aulas");
+            stage.show();
+
+        } catch (IOException e) {
+            System.out.println("Erro ao carregar a tela principal cadastro-aula.fxml!");
+            e.printStackTrace();
+        }
 
     }
 
     @FXML
     void entrar(ActionEvent event) {
-
-    }
-
-    @FXML
-    void esqueciSenha(ActionEvent event) {
 
     }
 
