f20b6bd366886f196efba330ad62fa17e4c529e4
Vincius <viniviniee11@gmail.com>
2026-05-30 00:35:03 -0300

    feat: cria o método salvar na DAO para salvar a matéria

--- api-2sem/src/main/java/org/datasphere/dao/MateriaDAO.java
+++ api-2sem/src/main/java/org/datasphere/dao/MateriaDAO.java
@@ -1,12 +1,10 @@
 package org.datasphere.dao;
 
+import org.datasphere.dao.interfaces.IDAO;
 import org.datasphere.database.ConexaoDB;
 import org.datasphere.model.MateriaModel;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,11 +29,6 @@ public class MateriaDAO {
         return materia;
     }
 
-    /**
-     * Lista todas as matérias vinculadas a um professor pelo e-mail.
-     * Um professor pode ter mais de uma matéria (ex.: professor substituto ou
-     * semestres distintos), por isso retorna uma lista.
-     */
     public List<MateriaModel> listarPorEmailProfessor(String email) {
         String sql = "SELECT * FROM materia WHERE email_professor = ?";
         List<MateriaModel> materias = new ArrayList<>();
@@ -63,4 +56,24 @@ public class MateriaDAO {
         materia.setEmailProfessor(rs.getString("email_professor"));
         return materia;
     }
+
+    public void salvar(MateriaModel materiaModel) {
+        String sql = "INSERT INTO materia (sigla, titulo, carga_horaria, email_professor) VALUES (?,?, ?, ?)";
+        try (Connection conn = ConexaoDB.getConexao()) {
+
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setString(1, materiaModel.getSigla());
+            ps.setString(2, materiaModel.getTitulo());
+            ps.setInt(3, materiaModel.getCargaHoraria());
+            ps.setString(4, materiaModel.getEmailProfessor());
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
