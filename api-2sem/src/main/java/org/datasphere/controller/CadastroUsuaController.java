22e9541052d2711a4656c8980cd5ad3bda6ec192
Vincius <viniviniee11@gmail.com>
2026-05-31 16:03:43 -0300

    fix: corrige a lógica de verificar se as senhas são iguais

--- api-2sem/src/main/java/org/datasphere/controller/CadastroUsuaController.java
+++ api-2sem/src/main/java/org/datasphere/controller/CadastroUsuaController.java
@@ -63,24 +63,25 @@ public class CadastroUsuaController {
 
     @FXML
     void cadastrarUsua(ActionEvent event) {
-        ProfessorModel professorCadastrado = new ProfessorModel();
-
-        if (!txtSenha.getText().equals(txtConfirmarSenha.getText())){
-            new Alert(Alert.AlertType.ERROR, "As senhas não são iguais", ButtonType.OK).show();
+        if (txtNome.getText().isBlank() || txtEmail.getText().isBlank() || txtSenha.getText().isBlank()) {
+            new Alert(Alert.AlertType.ERROR, "Preencha todos os campos.", ButtonType.OK).show();
+            return;
+        }
+        if (!txtSenha.getText().equals(txtConfirmarSenha.getText())) {
+            new Alert(Alert.AlertType.ERROR, "As senhas não são iguais.", ButtonType.OK).show();
             txtSenha.clear();
             txtConfirmarSenha.clear();
-        }
-        if (txtSenha.getText().equals(txtConfirmarSenha.getText())){
-            professorCadastrado.setNome(txtNome.getText());
-            professorCadastrado.setEmail(txtEmail.getText());
-            professorCadastrado.setSenha(txtSenha.getText());
-
-            cadastroDAO.cadastrar(professorCadastrado);
-            new Alert(Alert.AlertType.CONFIRMATION, "Cadastro concluído com sucesso", ButtonType.OK).show();
-
-            mudarTela(event,"/org/datasphere/login.fxml", "SIGA.ME");
+            return;
         }
 
+        ProfessorModel professorCadastrado = new ProfessorModel();
+        professorCadastrado.setNome(txtNome.getText());
+        professorCadastrado.setEmail(txtEmail.getText());
+        professorCadastrado.setSenha(txtSenha.getText());
+
+        cadastroDAO.cadastrar(professorCadastrado);
+        new Alert(Alert.AlertType.CONFIRMATION, "Cadastro concluído com sucesso!", ButtonType.OK).show();
+        mudarTela(event, "/org/datasphere/login.fxml", "SIGA.ME");
     }
 
 
